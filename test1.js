661e001b4ddad19042178210db3bb52211ad0de1
Enric Vergara <donkikochan@gmail.com>
2024-05-08 21:20:48 +0200

    ddd

---
+++ test1.js
@@ -0,0 +1,17 @@
+/*
+Fes un programa que, donat el radi d’una circumferència, calculi, a una nova variable i escrigui a la consola, la longitud de la circumferència i l’àrea del cercle. Farem servir una constant per guardar el número pi (3,141593)
+
+a) Longitud de la circumferència: 2 * PI * ràdio
+b) Àrea del cercle: PI * radio^2
+
+Passos
+1. Demana el radi
+    Fent servir la funció prompt() demana el radi i guarda-ho a una variable
+2. Escriu la circumferència
+    Fes el càlcul de la circumferència i escriu-lo a la consola. Pots fer-ho en una sola instrucció o en dues (guardant el resultat en una variable i escrivint després el contingut d'aquesta variable).
+3. Escriu l'àrea
+    Fes el càlcul de l'àrea i escriu-lo a la consola. Pots fer-ho en una sola instrucció o en dues (guardant el resultat en una variable i escrivint després el contingut d'aquesta variable).
+*/
+const PI = 3.141593;
+
+
